f3605209cc8c0ac8368a127ccde18336672c1160
vamshinasani <vamshinasani8371@gmail.com>
2024-04-21 18:26:27 +0530

    modified routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+import { HashRouter as Router } from 'react-router-dom';
+ReactDOM.render(
+  <Router>
+    <App/>
+  </Router>
+  , document.getElementById("root")
 );
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <HashRouter>
+//     <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   </HashRouter>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
