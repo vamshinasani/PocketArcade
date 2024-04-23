c0094d432a297a6b3298f4f8ecb95db0bfabe3f9
vamshinasani <vamshinasani8371@gmail.com>
2024-04-23 21:50:26 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
 import TicTacToe from './games/TicTacToe';
 import DotsAndConnect from './games/DotsAndConnect';
 import Home from './games/Home';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+// import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const routes = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/">
-        <Route index element={<Home />} />
+  // const routes = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/">
+  // <Route index element={<Home />} />
+  // <Route path='/tictactoe' element={<TicTacToe />} />
+  // <Route path='/dotsandconnect' element={<DotsAndConnect />} />
+  //     </Route>)
+  // )
+  return (
+      <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/tictactoe' element={<TicTacToe />} />
         <Route path='/dotsandconnect' element={<DotsAndConnect />} />
-      </Route>)
-  )
-  return (
-    <div className="App">
-      <RouterProvider router={routes}></RouterProvider>
-    </div>
+      </Routes>
   );
 }
 
