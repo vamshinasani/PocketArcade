fa547e9098fb487fe0bf31ffdb242ceb6b891856
vamshinasani <vamshinasani8371@gmail.com>
2024-04-23 18:22:08 +0530

    edited routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import TicTacToe from './games/TicTacToe';
 import DotsAndConnect from './games/DotsAndConnect';
 import Home from './games/Home';
-import { RouterProvider,Route,createBrowserRouter,createRoutesFromChildren } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const routes = createBrowserRouter(createRoutesFromChildren(
-    <Route path='/'>
-      <Route index element={<Home/>}></Route>
-      <Route path='/tictactoe' element={<TicTacToe/>}></Route>
-      <Route path='/dotsandconnect' element={<DotsAndConnect/>}></Route>
-    </Route>
-  ))
   return (
     <div className="App">
-      <RouterProvider router={routes}></RouterProvider>
+      <BrowserRouter basename='/PocketArcade'>
+        <Routes>
+          <Route exact path='/PocketArcade' element={<Home />}></Route>
+          <Route path='/tictactoe' element={<TicTacToe />}></Route>
+          <Route path='/dotsandconnect' element={<DotsAndConnect />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from 'react-router-dom';
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Router>
-    <App/>
+    <App />
   </Router>
-  , document.getElementById("root")
 );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <HashRouter>
-//     <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   </HashRouter>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
