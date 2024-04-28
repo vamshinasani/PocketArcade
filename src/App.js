50ca523d4b5654ccd31fb89fa4fb6912c2016b30
vamshinasani <vamshinasani8371@gmail.com>
2024-04-28 13:10:39 +0530

    changed route

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import './App.css';
 import TicTacToe from './games/TicTacToe';
 import DotsAndConnect from './games/DotsAndConnect';
 import Home from './games/Home';
-// import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  // const routes = createBrowserRouter(
-  //   createRoutesFromElements(
-  //     <Route path="/">
-  // <Route index element={<Home />} />
-  // <Route path='/tictactoe' element={<TicTacToe />} />
-  // <Route path='/dotsandconnect' element={<DotsAndConnect />} />
-  //     </Route>)
-  // )
   return (
       <Routes>
         <Route path='/' element={<Home />} />
